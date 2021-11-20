8bbb0262eec9d367c0386859c0e74d24b251dc42
EkeMinusYou <euonymuslke+develop@gmail.com>
2021-11-21 04:47:33 +0900

    this scope

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log('hello world');
+function main() {
+  const test = new Test();
+  const fn = test.returnValue;
+  console.log(fn()); // undefined
+
+  const arrowFn = test.arrowReturnValue;
+  console.log(arrowFn()); // undefined
+
+  const arrowFn2 = () => {
+    return test.returnValue();
+  }
+  console.log(arrowFn2()); // value
+
+  console.log(test.returnValue()); // value
+}
+
+class Test {
+  value = 'value';
+  returnValue() {
+    return this?.value;
+  }
+  arrowReturnValue = () => {
+    return this?.value;
+  }
+}
+
+main();
